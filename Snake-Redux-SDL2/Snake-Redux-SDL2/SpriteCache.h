e98ec7576ae4ef65807372b39a5f0ca7eca7f8c6
Isa Bolling <isaboll1@yahoo.com>
2018-09-29 15:00:07 -0400

    Add sprite cache

--- Snake-Redux-SDL2/Snake-Redux-SDL2/SpriteCache.h
+++ Snake-Redux-SDL2/Snake-Redux-SDL2/SpriteCache.h
@@ -5,6 +5,8 @@ using namespace std;
 
 class SpriteCache {
 private:
-
+	map<string, SDL_Texture *> sprite_cache;
+	SpriteCache();
 public:
+	SDL_Texture * Load_Sprites(SDL_Renderer * renderer, string filename);
 }cache;
