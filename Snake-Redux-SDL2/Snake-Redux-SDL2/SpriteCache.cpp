f5299ff4853720b5d87c6e50b4a4030bdf7c9915
Isa Bolling <isaboll1@yahoo.com>
2018-09-29 15:38:38 -0400

    Added Sprite Class

--- Snake-Redux-SDL2/Snake-Redux-SDL2/SpriteCache.cpp
+++ Snake-Redux-SDL2/Snake-Redux-SDL2/SpriteCache.cpp
@@ -1,9 +1,16 @@
 #include "SpriteCache.h"
 
+SpriteCache::~SpriteCache()
+{
+	for (auto item : sprite_cache) {
+		SDL_DestroyTexture(item.second);
+	}
+}
+
 SDL_Texture * SpriteCache::Load_Sprites(SDL_Renderer * renderer, string filename)
 {
 	if (!sprite_cache.count(filename)) {
-		SDL_Surface * surface = SDL_LoadBMP(filename.c_str);
+		SDL_Surface * surface = SDL_LoadBMP(filename.c_str());
 		sprite_cache[filename] = SDL_CreateTextureFromSurface(renderer, surface);
 		SDL_free(surface);
 	}
